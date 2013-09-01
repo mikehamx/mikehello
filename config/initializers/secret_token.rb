c8e22b464d851667040da37b565211aace1147d7
mikehamx <mike_hamill@baxter.com>
2013-09-01 21:10:17 +0200

    baseline

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mikehello::Application.config.secret_key_base = '61cd59b94f04e109442fbb195afd94e6b9bf2dec837cd940e7250b5ae4ed4da2e4ad516094a9bba7495c1be9933cdb1716588ff8b7eb23939ae36dd77f87ec3b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mikehello::Application.config.session_store :cookie_store, key: '_mikehello_session'
